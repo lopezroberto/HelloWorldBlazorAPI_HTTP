000ac3b7358862905060206fb06560dc803e7060
lopezrobert@outlook.com <lopezrobert@outlook.com>
2021-01-01 16:36:25 -0600

    Add project files.

---
+++ HelloWorldBlazorAPI_HTTP/Models/ISSLocation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldBlazorAPI_HTTP.Models
+{
+    public class ISSLocation
+    {
+        public int TimeStamp { get; set; }
+        public string Message { get; set; }
+        public ISSLocationPosition Iss_Position { get; set; }
+    }
+
+    public class ISSLocationPosition
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+
+}
