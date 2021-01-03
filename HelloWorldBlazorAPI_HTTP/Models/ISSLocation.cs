b5c364e997db2da3d1d6845560215be80766442d
lopezrobert@outlook.com <lopezrobert@outlook.com>
2021-01-02 18:58:16 -0600

    Delete _redirect needed?

--- HelloWorldBlazorAPI_HTTP/Models/ISSLocation.cs
+++ HelloWorldBlazorAPI_HTTP/Models/ISSLocation.cs
@@ -11,6 +11,15 @@ namespace HelloWorldBlazorAPI_HTTP.Models
         public int Id { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+        public decimal Altitude { get; set; }
+        public decimal Velocity { get; set; }
+        public string Visibility { get; set; }
+        public decimal Footprint { get; set; }
+        public int Timestamp { get; set; }
+        public decimal Daynum { get; set; }
+        public decimal Solar_lat { get; set; }
+        public decimal Solar_lon { get; set; }
+        public string Units{ get; set; }
     }
 
 }
